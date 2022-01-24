bf72fc694994c1e79fc486ca190b695683d066f1
Alessia9802 <alessiamisciagna@yahoo.it>
2022-01-24 15:48:35 +0100

    Method delete problem fixed

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -30,7 +30,6 @@
         </thead>
         <tbody>
             @foreach($articles as $article)
-           
             <tr>
                 <td scope="row">{{$article->id}}</td>
                 <td>{{$article->title}}</td>
@@ -46,33 +45,6 @@
                     <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete{{$article->id}}">
                         <i class="fas fa-trash"></i>
                     </button>
-
-                    <!-- Modal -->
-                    <div class="modal fade" id="delete{{$article->id}}" tabindex="-1" role="dialog" aria-labelledby="modal-{{$article->id}}" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title">Delete Article {{$article->title}}</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <p>
-                                        ⚡ Attenzione questa operazione non puo essere annullata!
-                                    </p>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <form action="{{route('admin.articles.destroy', $article->id)}}" method="post">
-                                        @csrf
-                                        @method('DELETE')
-
-                                        <button type="submit" class="btn btn-danger">Delete</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                 </td>
             </tr>
             @endforeach
@@ -82,5 +54,34 @@
 
     {{$articles->links()}}
 </div>
+
+
+@foreach($articles as $article)
+<!-- Modal -->
+<div class="modal fade" id="delete{{$article->id}}" tabindex="-1" role="dialog" aria-labelledby="modal-{{$article->id}}" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Delete Article {{$article->title}}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>
+                    ⚡ Attenzione questa operazione non puo essere annullata!
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <form action="{{route('admin.articles.destroy', $article->id)}}" method="post">
+                    @csrf
+                    @method('DELETE')
+
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endforeach
 
 @endsection
